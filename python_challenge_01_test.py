f6ee6503e4f886ddc6eadc43cf34e83c29a80cc8
dciurko <daria.ciurko@gmail.com>
2022-02-28 15:38:03 +0100

    Adding tests - test_guess_number_format_exception_during_guessing, test_prompting_messages_during_guessing,test_guess_number_range_min/max

--- python_challenge_01_test.py
+++ python_challenge_01_test.py
@@ -1,5 +1,6 @@
 import pytest
 import io
+import random
 import python_challenge_01
 
 
@@ -11,16 +12,40 @@ def test_guess_number_format_exception_as_first_step(monkeypatch, capfd):
     assert "It is not a number!" in out
 
 
-def test_guess_number_format_exception_during_guessing():
-    pass
+def test_guess_number_format_exception_during_guessing(monkeypatch, capfd):
+    drawn_number = random.randrange(0, 100)
+    monkeypatch.setattr('sys.stdin', io.StringIO(str(drawn_number - 5)))
+    python_challenge_01.fun_guess_number()
+    monkeypatch.setattr('sys.stdin', io.StringIO("A"))
+    with pytest.raises(ValueError):
+        python_challenge_01.fun_guess_number()
+    out, err = capfd.readouterr()
+    assert "It is not a number!" in out
 
 
-def test_guess_number_range_min():
-    pass
+def test_prompting_messages_during_guessing(capfd):
+    drawn_number = random.randrange(0, 100)
+    python_challenge_01.fun_validation(50)
+    out, err = capfd.readouterr()
+
+    if drawn_number < 50:
+        assert "Your number is too small!" in out
+    else:
+        assert "Your number is too high!" in out
 
 
-def test_guess_number_range_max():
-    pass
+def test_guess_number_range_min(capfd):
+    min_guess_number = random.randrange(-100, -2)
+    python_challenge_01.fun_validation(min_guess_number)
+    out, err = capfd.readouterr()
+    assert f"Your value {min_guess_number} is out of range!" in out
+
+
+def test_guess_number_range_max(capfd):
+    max_guess_number = random.randrange(101, 200)
+    python_challenge_01.fun_validation(max_guess_number)
+    out, err = capfd.readouterr()
+    assert f"Your value {max_guess_number} is out of range!" in out
 
 
 def test_exit_function_as_first_step():
