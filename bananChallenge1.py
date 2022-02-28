d2a13a8fc8dea5aad721357934a51caf245975f7
dciurko <daria.ciurko@gmail.com>
2022-02-28 09:19:53 +0100

    Assigning "__main__" to the __name__ variable.

--- bananChallenge1.py
+++ bananChallenge1.py
@@ -5,6 +5,7 @@ import sys
 
 drawn_number = random.randrange(0, 100)
 counter = 1
+is_valid = True
 
 
 def fun_guess_number():
@@ -17,7 +18,7 @@ def fun_guess_number():
 
 
 def fun_validation(number_to_validation):
-    is_valid = True
+    global is_valid
     if number_to_validation > 101 or number_to_validation < -1:
         print(f"Your value {number_to_validation} is out of range!")
         is_valid = False
@@ -36,15 +37,16 @@ def fun_exit():
         sys.exit(0)
 
 
-guess_number = fun_guess_number()
+if __name__ == "__main__":
+    guess_number = fun_guess_number()
 
-while guess_number != drawn_number:
-    fun_exit()
-    if fun_validation(guess_number):
-        guess_number = fun_guess_number()
-        counter += 1
-    else:
-        guess_number = fun_guess_number()
+    while guess_number != drawn_number:
+        fun_exit()
+        if fun_validation(guess_number):
+            guess_number = fun_guess_number()
+            counter += 1
+        else:
+            guess_number = fun_guess_number()
 
-    if guess_number == drawn_number:
-        print(f"You guessed! That's the number {drawn_number}! You hit in {counter} shots.")
+        if guess_number == drawn_number:
+            print(f"You guessed! That's the number {drawn_number}! You hit in {counter} shots.")
